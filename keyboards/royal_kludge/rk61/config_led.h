065cb2c5e1187fa336e41fe9cf8adb71cc6ca396
Diff-fusion <47172717+Diff-fusion@users.noreply.github.com>
2020-12-30 01:11:50 +0100

    Add RK61

---
+++ keyboards/royal_kludge/rk61/config_led.h
@@ -0,0 +1,29 @@
+/*
+Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Backlight configuration
+#define BACKLIGHT_LEVELS 8
+
+#define LED_MATRIX_ROWS 5
+#define LED_MATRIX_COLS 14
+#define LED_MATRIX_ROW_PINS { C1, C2, D4, C9, C10 }
+
+#define LED_DRIVER_LED_COUNT (LED_MATRIX_ROWS * LED_MATRIX_COLS)
+
+#define LED_MATRIX_MAXIMUM_BRIGHTNESS 255
---
+++ keyboards/royal_kludge/rk61/readme.md
@@ -0,0 +1,13 @@
+# RK61 Mechanical keyboard
+
+RK61
+
+Based on redragon k556 by: [Adam Honse](https://github.com/CalcProgrammer1)
+Based on Kemove DK63 by: [Stephen Peery](https://github.com/smp4488)
+Hardware Supported: SN32F260
+
+Make example for this keyboard (after setting up your build environment):
+
+    make royal_kludge/rk61:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
